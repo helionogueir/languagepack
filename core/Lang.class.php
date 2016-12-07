100748e29bf7395569fd19276936c11c87e8b307
helionogueir <helio.nogueir@gmail.com>
2016-12-07 11:10:10 -0200

    Start Version

--- core/Lang.class.php
+++ core/Lang.class.php
@@ -2,83 +2,69 @@
 
 namespace helionogueir\languagepack;
 
-use helionogueir\typeBoxing\type\String;
+use stdClass;
+use SplFileObject;
 use helionogueir\foldercreator\tool\Path;
-use helionogueir\typeBoxing\type\json\Decode;
 use helionogueir\filecreator\data\ReplaceText;
 
 /**
- * Lang control:
- * - Control language packages;
- *
+ * - Manipulate language package
  * @author Helio Nogueira <helio.nogueir@gmail.com>
- * @version v1.0.0
+ * @version v1.1.0
  */
-class Lang {
+abstract class Lang {
 
-  const SMARTY_MODIFIER_PATH = __DIR__ . DIRECTORY_SEPARATOR . 'modifier';
+  /**
+   * - Define standard locate
+   */
+  const STANDARD_LOCALE = 'en-US';
 
-  private static $locale = 'en-US';
+  private static $locale = Lang::STANDARD_LOCALE;
   private static $root = Array();
 
   /**
-   * Block construct the class, because this class is static
-   * @return false
-   */
-  public function __construct() {
-    return false;
-  }
-
-  /**
-   * Configuration lang:
-   * - Set locate
-   * 
-   * @param helionogueir\typeBoxing\type\String $locale Define locate of language
+   * - Define configuration locate of language
+   * @param string $locale Locate name of language
    * @return null
    */
-  public static final function configuration(String $locale) {
-    if (!$locale->isEmpty()) {
-      Lang::$locale = "{$locale}";
-    }
-    if (!$root->isEmpty()) {
-      Lang::$root = "{$root}";
+  public static final function configuration(string $locale) {
+    if (!empty($locale)) {
+      Lang::$locale = $locale;
+    } else {
+      Lang::$locale = Lang::STANDARD_LOCALE;
     }
     return null;
   }
 
   /**
-   * Add root finder:
    * - Define root of find of packages
-   * 
-   * @param helionogueir\typeBoxing\type\String $package Define root package of language
-   * @param helionogueir\typeBoxing\type\String $path Define path of root package of langauge
+   * @param string $package Root name of package language
+   * @param string $path Pathname of package language
    * @return null
    */
-  public static final function addRoot(String $package, String $path) {
-    if (!$package->isEmpty() && !$path->isEmpty()) {
-      $dirname = realpath($path);
+  public static final function addRoot(string $package, string $path) {
+    if (!empty($package) && !empty($path)) {
+      $dirname = realpath(Path::replaceOSSeparator($path));
       if (is_dir($dirname)) {
-        self::$root["{$package}"] = "{$dirname}";
+        Lang::$root[$package] = $dirname;
       }
     }
     return null;
   }
 
   /**
-   * Get language:
-   * - Identify lang and package and return text;
-   * 
-   * @param helionogueir\typeBoxing\type\String $identify Identify of language
-   * @param helionogueir\typeBoxing\type\String $package Package of language
-   * @param Array $data Data repalce of language
-   * @return helionogueir\typeBoxing\type\String Return text langauge
+   * - Identify TAG and package, and return text
+   * @param string $identify TAG identify language
+   * @param string $package Package language
+   * @param Array $data Data repalce language
+   * @return null
    */
-  public static final function get(String $identify, String $package, Array $data = null) {
+  public static final function get(string $identify, string $package, Array $data = null): string {
     $text = $identify;
-    if (!$identify->isEmpty()) {
-      if ($language = self::loadPackage($package)) {
+    if (!empty($identify)) {
+      if ($language = Lang::loadPackage($package)) {
         if (isset($language->{$identify})) {
-          $text = self::replace(new String($language->{$identify}), $data);
+          $text = Lang::replace($language->{$identify}, $data);
         }
       }
     }
@@ -86,28 +72,26 @@ class Lang {
   }
 
   /**
-   * Load package:
-   * - Identify and load package language;
-   * 
-   * @param helionogueir\typeBoxing\type\String $package Package of language
-   * @return stdClass Return package of language
+   * - Identify package and load package language
+   * @param string $package Path name package language 
+   * @return stdClass Object with language translate
    */
-  private static function loadPackage(String $package) {
-    $language = null;
-    if (!$package->isEmpty()) {
+  private static function loadPackage(string $package): stdClass {
+    $language = new stdClass();
+    if (!empty($package)) {
       $filename = null;
       if ($packageData = explode(":", $package)) {
         if (!empty($packageData[0])) {
-          $filename .= self::$root[$packageData[0]];
+          $filename .= Lang::$root[$packageData[0]];
         }
         if (!empty($packageData[1])) {
           $filename .= DIRECTORY_SEPARATOR . $packageData[1];
         }
-        $filename .= DIRECTORY_SEPARATOR . 'lang' . DIRECTORY_SEPARATOR . Lang::$locale . ".json";
-        if (file_exists($filename)) {
-          $filename = Path::replaceOSSeparator(new String($filename));
-          $decode = new Decode(file_get_contents($filename));
-          $language = $decode->getObject();
+        $pathaname = Lang::selectPackage($filename);
+        if (file_exists($pathaname)) {
+          $pathaname = Path::replaceOSSeparator($pathaname);
+          $decode = json_decode(file_get_contents((new SplFileObject($pathaname, "r"))->getPathname()));
+          $language = (JSON_ERROR_NONE == json_last_error()) ? $decode : $language;
         }
       }
     }
@@ -115,21 +99,31 @@ class Lang {
   }
 
   /**
-   * Relace data:
-   * - Replace data in language;
-   * 
-   * @param helionogueir\typeBoxing\type\String $text Text language
-   * @param helionogueir\typeBoxing\type\String $package Package of language
-   * @param Array $data Data repalce of language
-   * @return helionogueir\typeBoxing\type\String Return text langauge
+   * - Select package defined in configuration, case not exist package select standard package
+   * @param string $pathname Pathname of package language
+   * @return sting Pathname of package language
    */
-  private static function replace(String $text, Array $data = null) {
-    if (!$text->isEmpty() && count($data)) {
-      $metaText = $text;
-      foreach ($data as $key => $value) {
-        $metaText = preg_replace("#(\{data\:{$key}\})#si", $value, $metaText);
+  private static function selectPackage(string $pathname): string {
+    $filename = $pathname . DIRECTORY_SEPARATOR . 'lang' . DIRECTORY_SEPARATOR . Lang::$locale . ".json";
+    if (file_exists($filename)) {
+      $pathname = $filename;
+    } else {
+      $pathname .= DIRECTORY_SEPARATOR . 'lang' . DIRECTORY_SEPARATOR . Lang::STANDARD_LOCALE . ".json";
+    }
+    return $pathname;
+  }
+
+  /**
+   * - Replace TAGs in text
+   * @param string $text Text with TAG(s)
+   * @param string $data TAG(s) value(s)
+   * @return sting Text TAG(s) replaced
+   */
+  private static function replace(string $text, Array $data = null): string {
+    if (!empty($text) && count($data)) {
+      if ($metaText = (new ReplaceText())->replace($text, $data)) {
+        $text = $metaText;
       }
-      $text = new String($metaText);
     }
     return $text;
   }
---
+++ tests/unit/SmartyModifierLanguagepackLangTest.class.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace helionogueir\languagepack\tests\unit;
+
+use PHPUnit\Framework\TestCase;
+
+class SmartyModifierLanguagepackLangTest extends TestCase {
+
+  public function testModifier() {
+    require_once dirname(dirname(dirname(__FILE__)))
+        . DIRECTORY_SEPARATOR . "core"
+        . DIRECTORY_SEPARATOR . "modifier"
+        . DIRECTORY_SEPARATOR . "modifier.languagepack_lang.php";
+    $text = "Language Pack Test GET";
+    $this->assertEquals($text, smarty_modifier_languagepack_lang("languagepack:test:get", "helionogueir/languagepack", Array("method" => "GET")));
+  }
+
+  public function testModifierFail() {
+    require_once dirname(dirname(dirname(__FILE__)))
+        . DIRECTORY_SEPARATOR . "core"
+        . DIRECTORY_SEPARATOR . "modifier"
+        . DIRECTORY_SEPARATOR . "modifier.languagepack_lang.php";
+    $tag = "languagepack:test:get:fail";
+    $this->assertEquals($tag, smarty_modifier_languagepack_lang($tag, "helionogueir/languagepack"));
+  }
+
+}
